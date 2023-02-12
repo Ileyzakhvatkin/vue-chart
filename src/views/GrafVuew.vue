2a653567800792bae778cf116844315b0af4fabd
Ilya Zakhvatkin <iley.zakhvatkin@yandex.ru>
2023-02-12 05:45:46 +0300

    V_1_1

--- src/views/GrafVuew.vue
+++ src/views/GrafVuew.vue
@@ -1,19 +1,115 @@
 <template>
   <div class="sidebar-module sidebar-module-inset">
-    <h4>{{ name }}</h4>
-    <div class="graf">Тут будет график</div>
+    <h4>Vue Graf Example</h4>
+    <Doughnut
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+    />
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">Все расходы с 12.02.2023</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in tableComputed" :key="row.id">
+          <td>{{ row.title }}</td>
+          <td>{{ row.price }} ₽</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th class="text-right">ИТОГО:</th>
+          <th>0 ₽</th>
+        </tr>
+      </tfoot>
+    </table>
   </div>
 </template>
 
 <script>
-import grafDate from '../data/graf';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Doughnut } from 'vue-chartjs/legacy';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
 
 export default {
+  name: 'GrafVuew',
+  components: {
+    Doughnut,
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'doughnut-chart',
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label',
+    },
+    width: {
+      type: Number,
+      default: 300,
+    },
+    height: {
+      type: Number,
+      default: 300,
+    },
+    cssClasses: {
+      default: '',
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data() {
     return {
-      name: 'Vue Graf Example',
-      grafDate,
+      chartData: {
+        labels: ['ЦИАН', 'АВИТО', 'Яндекс', 'ДомКлик'],
+        datasets: [
+          {
+            backgroundColor: ['#5599d0', '#00aaff', '#e22b06', '#4bb053'],
+            data: [130, 80, 60, 10],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
     };
+  },
+  computed: {
+    tableComputed() {
+      const tableData = [];
+      this.chartData.labels.forEach((el, index) => {
+        tableData.push({ id: index, title: el, price: this.chartData.datasets[0].data[index] });
+      });
+      return tableData;
+    },
   },
   mounted() {
     console.log('Vue Graf Example mounted.');
--- src/views/StatisticView.vue
+++ src/views/StatisticView.vue
@@ -1,19 +1,101 @@
 <template>
   <div>
-    <h2 class="blog-post-title">{{ name }}</h2>
-    <div class="stat">Тут будет вывод статистики</div>
+    <h2 class="blog-post-title">Statiscic Example for RIS</h2>
+    <Bar
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+    />
   </div>
 </template>
 
 <script>
-import statDate from '../data/stat';
-// https://vue-chartjs.org/examples/#vue-3-charts
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Bar } from 'vue-chartjs/legacy';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
+  name: 'BarChart',
+  components: {
+    Bar,
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart',
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label',
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 400,
+    },
+    cssClasses: {
+      default: '',
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data() {
     return {
-      name: 'StatiscicExample',
-      statDate,
+      chartData: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
     };
   },
   mounted() {
